620b482bfecc5bd431cc70400bccee5c1c45790d
sangyeon kim <greyhairchooselife@gmail.com>
2023-03-13 16:50:33 +0900

    feat: data fetching api setting
      - mock데이터를 가져오는 api를 만들고,
    
      - 잘 받아오는지 간단히 테스트하였습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { useState, useEffect } from 'react';
+import productApi from '@/api/Flexsys';
+
 const App = () => {
-  return <div className="App">Template</div>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    productApi
+      .getProducts()
+      .then((res) => setData(res))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Flexsys data to graph</h1>
+      {!data ? 'Fetching Failed' : JSON.stringify(data)}
+    </div>
+  );
 };
 
 export default App;
