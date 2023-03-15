0ac7284c3a63541ecb2cabdbe4a0d209078f76b8
JUNU <bokjunwoo@gmail.com>
2023-03-16 01:10:39 +0900

    feat : 지역 클릭시에 CSS를 추가하는 기능 구현

--- src/components/ChartRegionSelect.tsx
+++ src/components/ChartRegionSelect.tsx
@@ -1,23 +1,39 @@
 import { IChartRegionSelectProps } from '@/interface/props';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const ChartRegionSelect = ({ region }: IChartRegionSelectProps) => {
   const navigate = useNavigate();
 
+  const [activeButton, setActiveButton] = useState('');
+
   const handleClick = (id: string) => {
+    setActiveButton(id);
     navigate(`?region=${id}`);
   };
 
+  const handleResetButton = () => {
+    setActiveButton('');
+    navigate('/');
+  };
+
   return (
-    <>
+    <div>
       {region.map((id) => {
         return (
-          <button key={id} onClick={() => handleClick(id)}>
+          <button
+            key={id}
+            onClick={() => handleClick(id)}
+            className={`btn ${activeButton === id ? 'active' : ''}`}
+          >
             {id}
           </button>
         );
       })}
-    </>
+      <button className="btn" onClick={handleResetButton}>
+        초기화
+      </button>
+    </div>
   );
 };
 
