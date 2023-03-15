718c5b25d1356c8885f6e58bc1233962d46c1cbe
양소연 <yangsoyeon@yangsoyeon-ui-MacBookPro-2.local>
2023-03-16 01:35:11 +0900

    feat: searchParams를 사용한 필터 리스트 선택 기능
    - 필터 목록의 아이템을 선택하면 react-router-dom에서 제공하는 useSearchParams 훅을 사용하여 query string에 등록될 수 있도록 구현했습니다.

--- src/components/ChartFilter.tsx
+++ src/components/ChartFilter.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSearchParams } from 'react-router-dom';
 import styles from './ChartFilter.module.css';
 
 type Props = {
@@ -6,15 +7,26 @@ type Props = {
 };
 
 const ChartFilter = ({ filterList }: Props) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    //TODO: set filter
+    const selectedId = e.target.name;
+    const idList = searchParams.getAll('id');
+
+    if (e.target.checked) {
+      setSearchParams({ id: [...idList, selectedId] });
+    } else {
+      const filtered = idList.filter((id) => id !== selectedId);
+      setSearchParams({ id: filtered });
+    }
   };
+
   return (
     <ul className={styles.filter_list}>
       {filterList.map((item, i) => (
         <li key={i} className={styles.filter_item}>
           <label htmlFor={item}>
-            <input type="checkbox" id={item} onChange={onChange} />
+            <input type="checkbox" id={item} name={item} onChange={onChange} />
             {item}
           </label>
         </li>
