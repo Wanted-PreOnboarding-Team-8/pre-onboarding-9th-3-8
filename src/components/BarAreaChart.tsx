2b4fb99db07dbc8abf5388da350f1c01925c92a7
sangyeon kim <greyhairchooselife@gmail.com>
2023-03-16 23:05:17 +0900

    feat: 필터링 기능) 데이터 구역 클릭 highlight
    - bar chart : 배경색 변경으로 표현
    
    - area chart : 데이터를 필터링하고 선으로 연결 해 표현
    
      highlight 표현 방식으로 dot만 표시하는게 시계열 차트에 적합하지 않다고
      생각해서 dot사이에 선을 그었습니다.
    
    - dot 사이에 라인을 그리는 것은 새로운 라인 차트를 그림으로써
      구현했습니다.
    
    - 이를 위해 차트를 그리면서 사용하는 데이터의 타입에 optional 속성을
      추가하였습니다. 참고 : @/interface/chartData.d.ts

--- src/components/BarAreaChart.tsx
+++ src/components/BarAreaChart.tsx
@@ -3,16 +3,37 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  Legend,
   Area,
   Bar,
+  Cell,
+  Line,
   ComposedChart,
   ResponsiveContainer,
 } from 'recharts';
+import { useState } from 'react';
 import CustomTooltip from '@/components/CustomTooltips';
+import { CategoricalChartState } from 'recharts/types/chart/generateCategoricalChart';
 import { IChartProps } from '@/interface/props';
 
 const BarAreaChart = ({ data, start, end }: IChartProps) => {
+  const [clickedRegion, setClickedRegion] = useState<string | null>(null);
+
+  if (clickedRegion) {
+    data = data.map((e) => {
+      return {
+        ...e,
+        value_highlight: e.id === clickedRegion ? e.value_area : null,
+      };
+    });
+  }
+
+  const onClickChart = (clicked: CategoricalChartState) => {
+    if (clicked?.activeLabel) {
+      const clickedData = data.find((e) => e.dateTime === clicked.activeLabel);
+      setClickedRegion(!clickedData ? null : clickedData.id);
+    }
+  };
+
   return (
     <>
       <h1>{`${start} ~ ${end}`}</h1>
@@ -25,9 +46,10 @@ const BarAreaChart = ({ data, start, end }: IChartProps) => {
               right: 30,
               left: 20,
             }}
+            onClick={onClickChart}
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="date" />
+            <XAxis dataKey="dateTime" />
             <YAxis
               yAxisId="left"
               label={{ value: 'value_bar', position: 'top', offset: 20 }}
@@ -42,20 +64,36 @@ const BarAreaChart = ({ data, start, end }: IChartProps) => {
               content={<CustomTooltip />}
               wrapperStyle={{ outline: 'none' }}
             />
-            <Legend />
-            <Bar
-              yAxisId="left"
-              dataKey="value_bar"
-              fill="#868e96"
-              barSize={20}
-            />
+            <Bar yAxisId="left" dataKey="value_bar" fill="#868e96" barSize={20}>
+              {data.map((e) => (
+                <Cell
+                  style={{ zIndex: 1 }}
+                  key={e.dateTime}
+                  fill={clickedRegion === e.id ? 'red' : '#868e96'}
+                  order={99}
+                />
+              ))}
+            </Bar>
             <Area
+              style={{ zIndex: 2 }}
               yAxisId="right"
               type="monotone"
               dataKey="value_area"
               stroke="#ff8787"
               fill="#ffa8a8"
+              animationDuration={500}
             />
+            {clickedRegion && (
+              <Line
+                yAxisId="right"
+                dataKey="value_highlight"
+                stroke="blue"
+                strokeWidth={3}
+                dot={{ stroke: 'blue', strokeWidth: 5 }}
+                connectNulls={true}
+                animationDuration={500}
+              />
+            )}
           </ComposedChart>
         </ResponsiveContainer>
       </div>
@@ -64,3 +102,9 @@ const BarAreaChart = ({ data, start, end }: IChartProps) => {
 };
 
 export default BarAreaChart;
+
+//  TODO
+//  private 분리, 옵션 먹인것 props로 전달받기
+//const StyledBarChart = (styleProp) => {}
+//const StyledAreaChart = (styleProp) => {}
+//const StyledHighlight = (styleProp) => {}
--- src/interface/chartData.d.ts
+++ src/interface/chartData.d.ts
@@ -15,6 +15,7 @@ interface IChartValue {
   id: string;
   value_area: number;
   value_bar: number;
+  value_highlight?: number | null; // 강조할 값을 추가 할 수 있음
 }
 
 export interface IChartDataset {
